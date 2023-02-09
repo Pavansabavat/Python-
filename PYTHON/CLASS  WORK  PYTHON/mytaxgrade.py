7a478b315000adf8e92e280e5f9f0f35fa43d3e9
Pavan Sabavat <124874683+Pavansabavat@users.noreply.github.com>
2023-02-09 15:18:03 +0530

    Add files via upload

---
+++ PYTHON/CLASS  WORK  PYTHON/mytaxgrade.py
@@ -0,0 +1,27 @@
+def yourtaxSlab(taxable_income):
+    if(taxable_income>=10):
+        tax=taxable_income*0.3
+    else:
+        if(taxable_income<10 and taxable_income>=5):
+            tax=taxable_income*0.2
+        else:
+            if(taxable_income<5and taxable_income>=2.5):
+                tax=taxable_income*0.05
+            else:
+                tax=0
+    return tax
+        
+def printgrade (score):
+    if score <0  or score > 100:
+        print("invalid score")
+    if score >=90.0:
+        print("A")
+    elif score >=80.0:
+        print("B")
+    elif score >=70.0:
+        print("C")
+    elif score>=60.0:
+        print("D")
+    else:
+        print('F')
+        
---
+++ PYTHON/CLASS  WORK  PYTHON/sumfrom.py
@@ -0,0 +1,4 @@
+def sumfrominputs():
+    num1=float(input("enter first number"))
+    num2=float(input("enter second number"))
+    print(num1+num2)
